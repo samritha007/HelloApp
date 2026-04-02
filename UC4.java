ce95c6ab9c8b1615da8dc956014501ed9a4360e8
nithi@Samritha <sam78892007@gmail.com>
2026-04-02 21:58:08 +0530

    Coded UC4

---
+++ UC4.java
@@ -0,0 +1,21 @@
+public class UC4{
+    public static void main(String[] args){
+        if (args.length == 0){
+            System.out.println("Hello,World!");
+        }
+        else{
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for(int i =0;i<args.length;i++){
+                nameBuilder.append(args[i]);
+
+                if(i<args.length - 1){
+                    nameBuilder.append(",");
+                }
+            }
+            String names = nameBuilder.toString();
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
